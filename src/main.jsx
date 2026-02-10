10bd5a6321da454cff1118fcde69dcebd3739e20
ayawazzani <2005120400095@ofppt-edu.ma>
2026-02-10 17:40:01 +0100

    Add .gitignore and clean up repository

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { BrowserRouter } from 'react-router-dom' // تأكد من استيرادها
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/QuickAnnonces-V1">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
